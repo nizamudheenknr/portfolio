ebfaec01fedef858333d48645b536e0045750d83
nizamudheenknr <151433533+nizamudheenknr@users.noreply.github.com>
2023-12-12 19:39:37 +0530

    Add files via upload

---
+++ portfolio2.js
@@ -0,0 +1,25 @@
+function sendMail(){
+     var params ={
+        name:  document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+
+        
+     }
+
+
+const serviceID = "service_f3sohwf";
+ const templateID = "template_dptmmpj"
+
+  emailjs.send(serviceID,templateID,params)
+  .then(
+        res =>{
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message send successfully");
+        }
+  )
+  .catch((err) => console.log(err));
+    }
